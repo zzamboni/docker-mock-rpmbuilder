6d05d8a534705f905fb418d86d0299ee12ddc845
Diego Zamboni <diego@zzamboni.org>
2015-06-24 15:24:20 -0500

    Improvement for image preparation and fewer arguments
    If neither `SOURCE_RPM` nor `SPEC_FILE` is specified, `mock init` is
    run, and at the end an option is given to checkpoint the image
    using `docker commit`. This makes it easier to create an image
    with mock already pre-configured, which makes future runs much
    faster.
    
    The `SOURCES` argument is now optional, `SPEC_FILE` is sufficient
    to start the build.

--- build-rpm.sh
+++ build-rpm.sh
@@ -10,9 +10,11 @@ if [ -z "$MOCK_CONFIG" ]; then
         ls -l $MOCK_CONF_FOLDER
 fi
 if [ -z "$SOURCE_RPM" ] && [ -z "$SPEC_FILE" ]; then
-        echo "You need to provide the src.rpm or spec file to build"
-        echo "Set SOURCE_RPM or SPEC_FILE environment variables"
-        exit 1
+	echo "Initializing mock...."
+	$MOCK_BIN -r $MOCK_CONFIG init
+	echo "Done - press Enter now to exit (you may want to 'docker commit' now if you are in docker)"
+	read x
+	exit
 fi
 
 #If proxy env variable is set, add the proxy value to the configuration file
@@ -44,15 +46,18 @@ if [ ! -z "$SOURCE_RPM" ]; then
         echo "========================================================================"
         $MOCK_BIN -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --resultdir=$OUTPUT_FOLDER
 elif [ ! -z "$SPEC_FILE" ]; then
-        if [ -z "$SOURCES" ]; then
-                echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
-                exit 1;
-        fi
+#        if [ -z "$SOURCES" ]; then
+#                echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
+#                exit 1;
+#        fi
         echo "      SPEC_FILE:     $SPEC_FILE"
-        echo "      SOURCES:       $SOURCES"
+#        echo "      SOURCES:       $SOURCES"
         echo "========================================================================"
-        $MOCK_BIN -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --resultdir=$OUTPUT_FOLDER
-        $MOCK_BIN -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --resultdir=$OUTPUT_FOLDER
+	OUTPUT=$(rpmbuild --define '_topdir /rpmbuild' -bs $MOUNT_POINT/$SPEC_FILE)
+        SRPM=$(echo $OUTPUT | awk '/^Wrote/ { print $2 }')
+        $MOCK_BIN -r $MOCK_CONFIG --rebuild $SRPM --resultdir=$OUTPUT_FOLDER
+        #$MOCK_BIN -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --resultdir=$OUTPUT_FOLDER
+        #$MOCK_BIN -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --resultdir=$OUTPUT_FOLDER
 fi
 
 echo "Build finished. Check results inside the mounted volume folder."
